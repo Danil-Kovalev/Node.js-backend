2ceac80f35ee67ebab3f91432f2fbedd92c9d8bc
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-09-07 22:33:29 +0300

    implemented methods: addSize, deleteSize, addReview

--- basicJS/task1/task.js
+++ basicJS/task1/task.js
@@ -1,19 +1,53 @@
 function Product() {
     
-    this.ID = "";
-    this.name = "";
-    this.description = "";
-    this.price = 0.0;
-    this.brand = "";
-    this.sizes = ["XS", "S", "M", "L", "XL", "XXL"];
-    this.activeSize = "";
-    this.quantity = 0;
-    this.date = Date;
-    this.images = [""];
-    this.reviews = {
-            ID: "",
-            author: "",
-            date: Date,
-            comment: "",
+    this.ID = "",
+    this.name = "",
+    this.description = "",
+    this.price = 0.0,
+    this.brand = "",
+    this.sizes = [],
+    this.activeSize = "",
+    this.quantity = 0,
+    this.date = Date,
+    this.images = [],
+    this.reviews = [
+        {
+            ID: this.ID,
+            author: this.name,
+            date: this.date,
+            comment: this.description,
+            rating: {
+                service: 0,
+                price: 0,
+                value: 0,
+                quality: 0
+            }
         }
-}
+    ]
+
+    this.getReviewByID = function(key) {
+        return this.reviews;
+    }
+
+    this.addSize = function(size) {
+        this.sizes.push(size);
+    }
+
+    this.deleteSize = function(key) {
+        let index = this.sizes.indexOf(key);
+        this.sizes.splice(index, 1);
+    }
+
+    this.addReview = function(key) {
+        this.reviews.fill(key);
+    }
+
+    this.deleteReview = function(key) {
+        let index = this.reviews.ID.indexOf(key);
+        this.reviews.splice(index, 1);
+    }
+
+    this.getAverageRating = function() {
+        return (rating.service + rating.price + rating.value + rating.quality) / 4;
+    }
+};
