552662d5903724559e68cca09eed0eeffe71e53e
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-09-14 17:27:18 +0300

    Fixed method getAverageRating. Completed functions sortProducts and searchProducts

--- basicJS/task1/task.js
+++ basicJS/task1/task.js
@@ -13,84 +13,84 @@ function Product() {
     this.reviews = []
 
     // create setters and getters for all fields
-    this.setID = function(ID) {
+    this.setID = function (ID) {
         this.ID = ID;
     }
 
-    this.getID = function() {
+    this.getID = function () {
         return this.ID;
     }
 
-    this.setName = function(name) {
+    this.setName = function (name) {
         this.name = name;
     }
 
-    this.getName = function() {
+    this.getName = function () {
         return this.name;
     }
 
-    this.setDescription = function(description) {
+    this.setDescription = function (description) {
         this.description = description;
     }
 
-    this.getDescription = function() {
+    this.getDescription = function () {
         return this.description;
     }
 
-    this.setPrice = function(price) {
+    this.setPrice = function (price) {
         this.price = price;
     }
 
-    this.getPrice = function() {
+    this.getPrice = function () {
         return this.price;
     }
 
-    this.setBrand= function(brand) {
+    this.setBrand = function (brand) {
         this.brand = brand;
     }
 
-    this.getBrand = function() {
+    this.getBrand = function () {
         return this.brand;
     }
 
-    this.setSizes = function(sizes) {
+    this.setSizes = function (sizes) {
         this.sizes = sizes;
     }
 
-    this.getSizes = function() {
+    this.getSizes = function () {
         return this.sizes;
     }
 
-    this.setActiveSize = function(activeSize) {
+    this.setActiveSize = function (activeSize) {
         this.activeSize = activeSize;
     }
 
-    this.getActiveSIze = function() {
+    this.getActiveSIze = function () {
         return this.activeSize;
     }
 
-    this.setQuantity = function(quantity) {
+    this.setQuantity = function (quantity) {
         this.quantity = quantity;
     }
 
-    this.getQuantity = function() {
+    this.getQuantity = function () {
         return this.quantity;
     }
 
-    this.setDate = function(date) {
+    this.setDate = function (date) {
         this.date = date;
     }
 
-    this.getDate = function() {
+    this.getDate = function () {
         return this.date;
     }
 
-    this.setImage = function(image) {
+    this.setImage = function (image) {
         this.images.push(image);
     }
-    
+
     //methods for all fields
-    this.getReviewByID = function(key) {
+    this.getReviewByID = function (key) {
         for (let index = 0; index < this.reviews.length; i++) {
             if (this.reviews[index].ID == key) {
                 return this.reviews[index];
@@ -98,7 +98,7 @@ function Product() {
         }
     }
 
-    this.getImage = function(key) {  //not working
+    this.getImage = function (key) {  //not working
         if (key != undefined) {
             const imageOut = document.querySelector('.image-out');
             let img = document.createElement('img');
@@ -111,29 +111,28 @@ function Product() {
         }
     }
 
-    this.addSize = function(size) {
+    this.addSize = function (size) {
         this.sizes.push(size);
     }
 
-    this.deleteSize = function(key) {
-        let index = this.sizes.indexOf(key);
-        this.sizes.splice(index, 1);
+    this.deleteSize = function (key) {
+        this.sizes.splice(this.sizes.indexOf(key), 1);
     }
 
-    this.addReview = function(review) {
+    this.addReview = function (review) {
         this.reviews.push(review);
     }
 
-    this.deleteReview = function(key) {
+    this.deleteReview = function (key) {
         for (let index = 0; index < this.reviews.length; index++) {
             if (this.reviews[index].ID == key) {
-                delete this.reviews[index];
+                this.reviews.splice(index, 1);
                 break;
             }
         }
     }
 
-    this.getAverageRating = function() {
+    this.getAverageRating = function () {
         let sumRating = 0;
         for (let index = 0; index < this.reviews.length; index++) {
             sumRating += this.reviews[index].rating.get("service");
@@ -141,49 +140,33 @@ function Product() {
             sumRating += this.reviews[index].rating.get("value");
             sumRating += this.reviews[index].rating.get("quality");
         }
-        return sumRating / this.reviews.length;
+        return sumRating / (this.reviews.length * this.reviews[0].rating.size);
     }
 }
 
-let shirt = new Product();
-let trainers = new Product();
-let review = {
-    ID: "213alex",
-    author: "alex",
-    date: 2023-8-12,
-    comment: "cool",
-    rating: new Map([
-        ["service", 5],
-        ["price", 1],
-        ["value", 3],
-        ["quality", 2]
-    ])
-}
-
-let review1 = {
-    ID: "123sam",
-    author: "sam",
-    date: 2022-1-13,
-    comment: "beautiful",
-    rating: new Map([
-        ["service", 1],
-        ["price", 2],
-        ["value", 3],
-        ["quality", 2]
-    ])
-}
-
-searchProducts = function(products, search) {
-
-}
-
-sortProducts = function (products, sortRule) {
-    if (sortRule == "price") {
-        for (let index = 0; index < products.length; index++) {
-            if (index + 1 < products.length && products[index].getPrice() > products[index + 1].getPrice()) {
-                [products[index], products[index + 1]] = [products[index + 1], products[index]];
-            }
+//Searches for all similar words or letters found in an array of objects
+searchProducts = function (products, search) {
+    let result = [];
+    for (let index = 0; index < products.length; index++) {
+        if (products[index].getName().toLowerCase().includes(search.toLowerCase())) {
+            result.push(products[index]);
+        }
+        else if (products[index].getDescription().toLowerCase().includes(search.toLowerCase())) {
+            result.push(products[index]);
         }
     }
-    return products;
+    return result;
+}
+
+//Sorting products by price, name, ID
+sortProducts = function (products, sortRule = "") {
+    if (sortRule == "price") {
+        return products.sort((firstElement, secondElement) => firstElement.price > secondElement.price ? 1 : -1)
+    }
+    else if (sortRule == "name") {
+        return products.sort((firstElement, secondElement) => firstElement.name > secondElement.name ? 1 : -1)
+    }
+    else {
+        return products.sort((firstElement, secondElement) => firstElement.ID > secondElement.ID ? 1 : -1)
+    }
 }
