7f97a69baa77a481830c22ab22c87b1f44c6bf1d
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-09-26 19:12:01 +0300

    raw fix display of reviews information. Change AbstractProduct style. Added method getReviews for display information

--- basicJS/OOP/oop.js
+++ basicJS/OOP/oop.js
@@ -12,126 +12,134 @@ function AbstractProduct() {
     this.images = [];
     this.reviews = []
 };
-Object.assign(AbstractProduct.prototype, {
-    setID(ID) {
-        this.ID = ID;
-    },
-    
-    getID() {
-        return this.ID;
-    },
-    
-    setName(name) {
-        this.name = name;
-    },
-    
-    getName() {
-        return this.name;
-    },
-    
-    setDescription(description) {
-        this.description = description;
-    },
-    
-    getDescription() {
-        return this.description;
-    },
-    
-    setPrice(price) {
-        this.price = price;
-    },
-    
-    getPrice() {
-        return this.price;
-    },
-    
-    setBrand(brand) {
-        this.brand = brand;
-    },
-    
-    getBrand() {
-        return this.brand;
-    },
-    
-    setQuantity(quantity) {
-        this.quantity = quantity;
-    },
-    
-    getQuantity() {
-        return this.quantity;
-    },
-    
-    setDate(date) {
-        this.date = date;
-    },
-    
-    getDate() {
-        return this.date;
-    },
-    
-    setImage(image) {
-        this.images.push(image);
-    },
-    
-    //methods for all fields
-    getReviewByID(key) {
-        for (let index = 0; index < this.reviews.length; i++) {
-            if (this.reviews[index].ID == key) {
-                return this.reviews[index];
-            }
-        }
-    },
-    
-    getImage(key) {
-        if (key == undefined) {
-            return this.images[0];
-        }
-        else {
-            return this.images[this.images.indexOf(key)];
-        }
-    },
-    
-    addSize(size) {
-        this.sizes.push(size);
-    },
-    
-    deleteSize(key) {
-        this.sizes.splice(this.sizes.indexOf(key), 1);
-    },
-    
-    addReview(review) {
-        this.reviews.push(review);
-    },
-    
-    deleteReview(key) {
-        for (let index = 0; index < this.reviews.length; index++) {
-            if (this.reviews[index].ID == key) {
-                this.reviews.splice(index, 1);
-                break;
-            }
-        }
-    },
-    
-    getAverageRating() {
-        let sumRating = 0;
-        for (let index = 0; index < this.reviews.length; index++) {
-            sumRating += this.reviews[index].rating.get("service");
-            sumRating += this.reviews[index].rating.get("price");
-            sumRating += this.reviews[index].rating.get("value");
-            sumRating += this.reviews[index].rating.get("quality");
-        }
-        return sumRating / (this.reviews.length * this.reviews[0].rating.size);
-    },
 
-    getFullInformation() {
+AbstractProduct.prototype.setID = function(ID) {
+    this.ID = ID;
+};
+
+AbstractProduct.prototype.getID = function() {
+    return this.ID;
+};
+
+AbstractProduct.prototype.setName = function(name) {
+    this.name = name;
+};
+
+AbstractProduct.prototype.getName = function() {
+    return this.name;
+};
+
+AbstractProduct.prototype.setDescription = function(description) {
+    this.description = description;
+};
+
+AbstractProduct.prototype.getDescription = function() {
+    return this.description;
+};
+
+AbstractProduct.prototype.setPrice = function(price) {
+    this.price = price;
+};
+
+AbstractProduct.prototype.getPrice = function() {
+    return this.price;
+};
+
+AbstractProduct.prototype.setBrand = function(brand) {
+    this.brand = brand;
+};
+
+AbstractProduct.prototype.getBrand = function() {
+    return this.brand;
+},
+
+AbstractProduct.prototype.setQuantity = function(quantity) {
+    this.quantity = quantity;
+};
+
+AbstractProduct.prototype.getQuantity = function() {
+    return this.quantity;
+};
+
+AbstractProduct.prototype.setDate = function(date) {
+    this.date = date;
+};
+
+AbstractProduct.prototype.getDate = function() {
+    return this.date;
+};
+
+AbstractProduct.prototype.setImage = function(image) {
+    this.images.push(image);
+};
+
+AbstractProduct.prototype.getImage = function(key) {
+    if (key == undefined) {
+        return this.images[0];
+    }
+    else {
+        return this.images[this.images.indexOf(key)];
+    }
+};
+
+//methods for all fields
+AbstractProduct.prototype.getReviewByID = function(key) {
+    for (let index = 0; index < this.reviews.length; i++) {
+        if (this.reviews[index].ID == key) {
+            return this.reviews[index];
+        }
+    }
+};
+
+AbstractProduct.prototype.addReview = function(review) {
+    this.reviews.push(review);
+};
+
+AbstractProduct.prototype.deleteReview = function(key) {
+    for (let index = 0; index < this.reviews.length; index++) {
+        if (this.reviews[index].ID == key) {
+            this.reviews.splice(index, 1);
+            break;
+        }
+    }
+};
+
+AbstractProduct.prototype.getReviews = function() {
+    let strReview = [];
+    for (let i = 0; i < this.reviews.length; i++) {
+        strReview.push(`\nID: ${this.reviews[i].ID}`);
+        strReview.push(` Author: ${this.reviews[i].author}`);
+        strReview.push(` Date: ${this.reviews[i].date}`);
+        strReview.push(` Comment: ${this.reviews[i].comment}`);
+        strReview.push(` Rating: ${this.reviews[i].rating}`);
+    }
+    return strReview;
+};
+
+AbstractProduct.prototype.getAverageRating = function() {
+    let sumRating = 0;
+    for (let index = 0; index < this.reviews.length; index++) {
+        sumRating += this.reviews[index].rating.get("service");
+        sumRating += this.reviews[index].rating.get("price");
+        sumRating += this.reviews[index].rating.get("value");
+        sumRating += this.reviews[index].rating.get("quality");
+    }
+    return sumRating / (this.reviews.length * this.reviews[0].rating.size);
+};
+
+AbstractProduct.prototype.getAllImages = function() {
+        return this.images;
+};
+
+AbstractProduct.prototype.getFullInformation = function() {
         console.log("ID: " + this.getID() + "\nName: " + this.getName() + "\nDescription: " + this.getDescription() +
         "\nPrice: " + this.getPrice() + "\nQuantity: " + this.getQuantity() + "\nDate: " + this.getDate() +
-        "\nBrand: " + this.getBrand() + "\nImage: " + this.getImage() + "\nReviews: " + this.reviews);
-    },
-    getPriceForQuantity() {
+        "\nBrand: " + this.getBrand() + "\nImage: " + this.getAllImages() + `\nReviews{${this.getReviews()}\n}`);
+};
 
-    }
-})
+AbstractProduct.prototype.getPriceForQuantity = function() {
+
+};
 
 function Clothes() {
     AbstractProduct.call(this);
@@ -140,6 +148,7 @@ function Clothes() {
 };
 Clothes.prototype = Object.create(AbstractProduct.prototype);
 Clothes.prototype.constructor = Clothes;
+
 Object.assign(Clothes.prototype, {
     setMaterial(material) {
         this.material = material;
@@ -170,6 +179,7 @@ function Electronics() {
 }
 Electronics.prototype = Object.create(AbstractProduct.prototype);
 Electronics.prototype.constructor = Electronics;
+
 Object.assign(Electronics.prototype, {
     setWarranty(warranty) {
         this.warranty = warranty;
@@ -192,7 +202,7 @@ Object.assign(Electronics.prototype, {
         console.log("Warranty: " + this.getWarranty() + "\nPower: " + this.getPower());
     }
 })
-
+    
 //Searches for all similar words or letters found in an array of objects
 searchProducts = function (products, search) {
     let result = [];
