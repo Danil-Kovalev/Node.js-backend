84423f7043d8f394dd913b216f167dae24eb95cc
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-09-30 23:10:24 +0300

    added functional: delete invalid string, make associative array, sort cities by population, choose the top-10 cities by population

--- basicJS/FP/fp.js
+++ basicJS/FP/fp.js
@@ -1,5 +1,16 @@
 getCSV = function(textCSV) {
-    let text = textCSV.split("\n").filter((element) => /^(#?\d{2}\.\d{2})?,(#?\d{2}\.\d{2})?,(#?[a-zA-Z]])*?,(#?\d)*?,$/g.test(element));
+    let text = textCSV.split("\n").filter((element) => /^(#?\d{0,2}\.\d{0,2})?,(#?\d{0,2}\.\d{0,2})?,(#?[a-zA-Z]*)?,(#?\d*)?,?$/g.test(element));
+    let informationCity = [];
+    text.map((elementText) => {
+        let arrayCity = {};
+        arrayCity.x = Number(elementText.match(/^(#?\d{0,2}\.\d{0,2})/g));
+        arrayCity.y = Number(elementText.match(/(?<=,)#?\d{0,2}\.\d{0,2}/g).toString());
+        arrayCity.name = elementText.match(/(?<=,)#?[a-zA-Z]*(?=,)/g).toString();
+        arrayCity.population = Number(elementText.match(/(?<=,)#?\d*(?=,)/g));
+        informationCity.push(arrayCity);
+    });
+    informationCity.sort((firstElement, secondElement) => firstElement.population < secondElement.population ? 1 : -1);
+    let topCity = informationCity.slice(0, 9);
 
     return getParsedCSV = function(text) {
         return text;
