4965482d64e2263a1e18ed22521a8304a69b4f86
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-10-01 20:12:20 +0300

    added raw realization reduce

--- basicJS/FP/fp.js
+++ basicJS/FP/fp.js
@@ -9,9 +9,16 @@ getCSV = function(textCSV) {
         arrayCity.population = Number(elementText.match(/(?<=,)#?\d*(?=,)/g));
         informationCity.push(arrayCity);
     });
-    informationCity.sort((firstElement, secondElement) => firstElement.population < secondElement.population ? 1 : -1);
-    let topCity = informationCity.slice(0, 9);
-
+    informationCity.sort((firstElement, secondElement) => firstElement.population < secondElement.population ? 1 : -1).slice(0, 9);
+    let ratingCities = informationCity.reduce((arrayRating, currentElement, indexElement) => {
+        arrayRating[arrayRating.name] = {population: arrayRating.population, rating: indexElement + 1};
+        arrayRating[currentElement.name] = {population: currentElement.population, rating: indexElement + 1};
+        return arrayRating;
+    });
+    delete ratingCities.x;
+    delete ratingCities.y;
+    delete ratingCities.name;
+    delete ratingCities.population;
     return getParsedCSV = function(text) {
         return text;
     };
