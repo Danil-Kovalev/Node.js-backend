874e235388f6b5085f36622f7b2bf0dce2d5d779
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-10-03 19:22:39 +0300

    fixed reduced and added conversion to one object from array of objects. Added showing full information of the city

--- basicJS/FP/fp.js
+++ basicJS/FP/fp.js
@@ -1,19 +1,23 @@
-getCSV = function(textCSV) {
-    let text = textCSV.split("\n").filter((element) => /^(#?\d{0,2}\.\d{0,2})?,(#?\d{0,2}\.\d{0,2})?,(#?[a-zA-Z]*)?,(#?\d*)?,?$/g.test(element));
+getCSV = function(CSV) {
+    let text = CSV.split("\n").filter((element) => /^(#?\d{0,2}\.\d{0,2})?,(#?\d{0,2}\.\d{0,2})?,(#?[a-zA-Z]*)?,(#?\d*)?,?$/g.test(element));
     let informationCity = [];
     text.map((elementText) => {
-        let arrayCity = {};
-        arrayCity.x = Number(elementText.match(/^(#?\d{0,2}\.\d{0,2})/g));
-        arrayCity.y = Number(elementText.match(/(?<=,)#?\d{0,2}\.\d{0,2}/g).toString());
-        arrayCity.name = elementText.match(/(?<=,)#?[a-zA-Z]*(?=,)/g).toString();
-        arrayCity.population = Number(elementText.match(/(?<=,)#?\d*(?=,)/g));
-        informationCity.push(arrayCity);
+        informationCity.push({
+        x: Number(elementText.match(/^(#?\d{0,2}\.\d{0,2})/g)),
+        y: Number(elementText.match(/(?<=,)#?\d{0,2}\.\d{0,2}/g).toString()),
+        name: elementText.match(/(?<=,)#?[a-zA-Z]*(?=,)/g).toString(),
+        population: Number(elementText.match(/(?<=,)#?\d*(?=,)/g))});
     });
     informationCity.sort((firstElement, secondElement) => firstElement.population < secondElement.population ? 1 : -1).slice(0, 9);
     let ratingCities = informationCity.reduce((previousElement, currentElement, indexElement) => {
         return previousElement[currentElement.name] = {population: currentElement.population, rating: indexElement + 1}, previousElement;
     },{});
-    return getParsedCSV = function(text) {
-        return text;
+    return () => {
+        let pattern = "\nName (Xst place in the top 10 largest cities in Ukraine, population Y people)";
+        let fullText = [];
+        Object.entries(ratingCities).forEach(([key, value]) => {
+        fullText.push(pattern.replace(/Name/g, key).replace(/X/g, value.rating).replace(/Y/g, value.population));
+        });
+        return fullText.toString();
     };
 };
