2236df3e2642b0609f9e4593b9e1befecb43005c
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-10-15 20:16:53 +0300

    added index.html, style.css, script.js files, which doing certain actions

---
+++ introducionToFrontendJS/script.js
@@ -0,0 +1,18 @@
+// hide with CSS
+function click1() {
+    document.getElementById("black-square").style.display = "none";
+};
+
+// hide with JS
+function click2() {
+    document.getElementById("black-square").remove();
+}
+
+// hide with CSS + JS
+function click3() {
+    document.getElementById("black-square").style.display = "none";
+}
+
+function click4() {
+    document.getElementById("black-square").style.display = "none";
+}
