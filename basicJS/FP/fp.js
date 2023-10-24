bc20260b231a100dcbc4da807e173849b82b6a7e
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-10-24 14:30:26 +0300

    added realization to anonymous function, which replaceces text with a pattern. And added show realization task

--- basicJS/FP/fp.js
+++ basicJS/FP/fp.js
@@ -12,12 +12,26 @@ getCSV = function(CSV) {
     let ratingCities = informationCity.reduce((previousElement, currentElement, indexElement) => {
         return previousElement[currentElement.name] = {population: currentElement.population, rating: indexElement + 1}, previousElement;
     },{});
-    return () => {
-        let pattern = "\nName (Xst place in the top 10 largest cities in Ukraine, population Y people)";
-        let fullText = [];
+    return (text) => {
+        let pattern = "Name (Xst place in the top 10 largest cities in Ukraine, population Y people)";
         Object.entries(ratingCities).forEach(([key, value]) => {
-        fullText.push(pattern.replace(/Name/g, key).replace(/X/g, value.rating).replace(/Y/g, value.population));
+            if (text.includes(key)) {
+                text = text.replaceAll(key, pattern.replace(/Name/g, key).replace(/X/g, value.rating).replace(/Y/g, value.population));
+            }
         });
-        return fullText.toString();
+        return text.toString();
     };
 };
+
+let text = getCSV("44.38,34.33,Kyiv,2884000000,\n49.46,30.17,Kropyvnytskyi,226491,\n54.13,43.90,Mykolayiv,486267,\n49.54,28.49,Kherson,289697," + 
+"\n21.34,54.86,Cherkasy,279074,\n12.54,31.53,Zaporizhya,746749,\n54.12,75.32,Vinnytsia,370834,\n79.14,62.23,Kryvyi Rih,646748," + 
+"\n75.12,56.87,Zhytomyr,261624,\n65.12,57.23,Uzhhorod,115542,\n54.76,56.23,Mukachevo,85903");
+
+let fullText = `Kropyvnytskyi – це батьківщина українського професійного театру.\nKyiv - дивно красиве місто, яке прекрасне незалежно від пори року.
+Mykolayiv має найдовшу пішохідну вулицю в Україні.\nМісто Kherson побудований на катакомбах.\nCherkasy мали замкову гору.
+Zaporizhya – місто, в якому досі відчувається козацький дух, попри сучасні забудову й індустріальні підприємства.
+Vinnytsia - місто танцюючих фонтанів.\nZhytomyr був третім містом України, в якому з’явився трамвай.
+Kryvyi Rih є одним із найдовших міст у всьому світі: його протяжність становить 126 кілометрів.
+Якщо древній Uzhhorod – це серце Закарпаття, то Mukachevo – його душа`;
+
+console.log(text(fullText));
