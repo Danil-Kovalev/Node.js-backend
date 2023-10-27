a659ea86ef87c7cf1d2a9e829ff7fad6e7cd32b4
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-10-27 21:05:38 +0300

    uncommented first task

--- lvl2/PromisesAwaitAsync/src/index.ts
+++ lvl2/PromisesAwaitAsync/src/index.ts
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch';
 
 //1
-// const response = await fetch("https://api.ipify.org?format=json");
-// const data = await response.json();
+const response = await fetch("https://api.ipify.org?format=json");
+const data = await response.json();
 
-// console.log(data as Promise<string>);
+console.log(data as Promise<string>);
 
 //2
 
