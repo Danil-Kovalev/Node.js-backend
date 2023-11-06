47072410369fc38b9a1a593ec2fce5686fa6ec25
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-11-06 19:56:06 +0200

    added import functions to CRUD

--- lvl2/RestMongo/src/serverV1.ts
+++ lvl2/RestMongo/src/serverV1.ts
@@ -3,6 +3,7 @@ import * as path from 'path';
 import bodyParser from "body-parser";
 import session from 'express-session';
 import FileStore from 'session-file-store';
+import {getData, addData, updateData, deleteData} from './functionCRUD'
 
 const app = express();
 const dirname = path.resolve();
@@ -26,16 +27,16 @@ app.get('/', (req: any, res: any) => {
 
 app.route('/api/v1/items')
   .get((req: any, res: any) => {
-    res.sendFile(path.resolve(dirname, 'data.json'));
+    res.send(getData());
   })
   .post((req: any, res: any) => {
-
+    res.send(addData(req));
   })
   .put((req: any, res: any) => {
-
+    res.send(updateData(JSON.parse(req)));
   })
   .delete((req: any, res: any) => {
-
+    res.sende(deleteData(JSON.parse(req)));
   })
 
 app.listen(port, () => {
