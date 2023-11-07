2fd16fb449ddaa6c834f3b0e97f342f8f0618238
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-11-07 19:35:24 +0200

    added processing requests to server(add, update, delete)

--- lvl2/RestMongo/src/functionCRUD.ts
+++ lvl2/RestMongo/src/functionCRUD.ts
@@ -11,18 +11,43 @@ export function getData() {
 export function addData(textUser: any) {
     let newData = {
         "id": data.items.length + 1,
-        "text": textUser,
+        "text": textUser.text.toString(),
         "checked": true
     }
     data.items.push(newData);
     fs.writeFileSync('data.json', JSON.stringify(data));
-    return JSON.stringify(newData.id);
+    return JSON.stringify({
+        "id": data.items.length
+    });
 }
 
-export function updateData(data: string) {
-
+export function updateData(dataUser: any) {
+    let idUser = JSON.stringify(dataUser.id);
+    let result = {
+        "ok": false
+    }
+    data.items.forEach((element: any) => {
+        if (element.id.toString() === idUser) {
+            data.items[element.id - 1] = dataUser;
+            fs.writeFileSync('data.json', JSON.stringify(data));
+            result.ok = true;
+            return JSON.stringify(result);
+        }
+    });
+    return JSON.stringify(result);
 }
 
-export function deleteData(data: string) {
-
+export function deleteData(dataUser: any) {
+        let result = {
+            "ok": false
+        }
+        data.items.forEach((element: any) => {
+            if (element.id === dataUser.id) {
+                data.items.splice(element.id - 1, 1);
+                fs.writeFileSync('data.json', JSON.stringify(data));
+                result.ok = true;
+                return JSON.stringify(result);
+            }
+        })
+        return JSON.stringify(result);
 }
