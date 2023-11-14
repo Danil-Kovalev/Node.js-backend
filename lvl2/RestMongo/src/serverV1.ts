cab12aae18025558b15acbfd7e7a8e88df9664d3
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-11-14 16:04:16 +0200

    added request processing for register, logout, login and added sessions

--- lvl2/RestMongo/src/serverV1.ts
+++ lvl2/RestMongo/src/serverV1.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import session from 'express-session';
 import FileStore from 'session-file-store';
 import {getData, addData, updateData, deleteData} from './functionCRUD'
+import { loginUser, registerUser } from './checkUsers';
 
 const app = express();
 const dirname = path.resolve();
@@ -14,13 +15,24 @@ const jsonParser = bodyParser.json();
 app.use(express.static(path.resolve(dirname, 'front')));
 app.use(
     session({
-      store: new FileStoreSession({}),
+      store: new FileStoreSession({
+        path: "./sessions"
+      }),
       secret: 'keyboard cat',
       resave: true,
       saveUninitialized: true,
-      cookie: {maxAge: 1800000}
+      cookie: {
+        path: "/",
+        maxAge: 1800000
+      }
     })
 );
+
+declare module "express-session" {
+  interface SessionData {
+    login: string
+  }
+}
 
 app.get('/', (req: Request, res: Response) => {
     res.sendFile(path.resolve(dirname, 'front', 'index.html'));
@@ -40,18 +52,34 @@ app.route('/api/v1/items')
     res.send(deleteData(req.body));
   })
 
-app.listen(port, () => {
-    console.log(`Server V1 starts on port ${port}`);
-});
-
 app.post('/api/v1/login', jsonParser, (req: Request, res: Response) => {
-  
+  if (loginUser(req.body)) {
+    res.send({"ok": true})
+  }
+  else {
+    res.send({"error": "User not exist"})
+  }
 })
 
-app.post('api/v1/logout', jsonParser, (req: Request, res: Response) => {
-  
+app.post('/api/v1/logout', (req: Request, res: Response) => {
+  req.session.destroy((err) => {
+    if (err) throw Error;
+    res.clearCookie('connect.sid');
+    res.send({"ok": true});
+  })
 })
 
-app.post('api/v1/register', jsonParser, (req: Request, res: Response) => {
-  
+app.post('/api/v1/register', jsonParser, (req: Request, res: Response) => {
+  if (registerUser(req.body)) {
+    let loginUser = req.body;
+    req.session.login = loginUser.login;
+    res.send({"ok": true})
+  }
+  else {
+    res.send({"error": "User exist"})
+  }
 })
+
+app.listen(port, () => {
+  console.log(`Server V1 starts on port ${port}`);
+});
