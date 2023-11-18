c2f9fbec7a26f3dff6b59007f4a648c46c27a729
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-11-18 20:01:23 +0200

    added connect and interaction with database

--- lvl2/RestMongo/src/functionCRUD.ts
+++ lvl2/RestMongo/src/functionCRUD.ts
@@ -1,20 +1,28 @@
-import * as path from 'path';
 import data from '../data.json';
 import * as fs from 'fs';
+import { MongoClient } from 'mongodb';
 
-const dirname = path.resolve();
+const dbUrl = "mongodb+srv://Danylo:passwordcourses12345@datauser.tqkhlcr.mongodb.net/";
+const client = new MongoClient(dbUrl);
+client.connect().then(res => console.log("Connected to database!"));
+const db = client.db("myDatabase");
+const collectionDb = db.collection("dataUsers");
 
 export function getData() {
+    collectionDb.countDocuments().then(result => {
+        if (result == 0) collectionDb.insertMany(data.items);
+    })
     return JSON.stringify(data);
 }
 
-export function addData(textUser: any) {
+export function addData(dataUser: any) {
     let newData = {
         "id": data.items.length + 1,
-        "text": textUser.text.toString(),
+        "text": dataUser.text.toString(),
         "checked": true
     }
     data.items.push(newData);
+    collectionDb.insertOne(newData);
     fs.writeFileSync('data.json', JSON.stringify(data));
     return JSON.stringify({
         "id": data.items.length
@@ -26,6 +34,7 @@ export function updateData(dataUser: any) {
     let result = {
         "ok": false
     }
+    collectionDb.updateOne({id: dataUser.id}, {$set: {text: dataUser.text.toString()}});
     data.items.forEach((element: any) => {
         if (element.id.toString() === idUser) {
             data.items[element.id - 1] = dataUser;
@@ -38,16 +47,17 @@ export function updateData(dataUser: any) {
 }
 
 export function deleteData(dataUser: any) {
-        let result = {
-            "ok": false
+    let result = {
+        "ok": false
+    }
+    collectionDb.deleteOne({id: dataUser.id});
+    data.items.forEach((element: any) => {
+        if (element.id === dataUser.id) {
+            data.items.splice(element.id - 1, 1);
+            fs.writeFileSync('data.json', JSON.stringify(data));
+            result.ok = true;
+            return JSON.stringify(result);
         }
-        data.items.forEach((element: any) => {
-            if (element.id === dataUser.id) {
-                data.items.splice(element.id - 1, 1);
-                fs.writeFileSync('data.json', JSON.stringify(data));
-                result.ok = true;
-                return JSON.stringify(result);
-            }
-        })
-        return JSON.stringify(result);
+    })
+    return JSON.stringify(result);
 }
