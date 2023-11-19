3898feffd3c50960a6eaca032b921f2a8cce0872
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-11-19 19:12:10 +0200

    modified registration and login functions

--- lvl2/RestMongo/src/checkUsers.ts
+++ lvl2/RestMongo/src/checkUsers.ts
@@ -1,16 +1,26 @@
 import dataUsers from '../users.json';
 import * as fs from 'fs';
 
+export function getUser(login: string) {
+    dataUsers.users.map((element: any) => {
+        element.login === login
+    })
+}
+
 export function loginUser(dataUser: any) {
-    return dataUsers.users.find((element: any) => element.login === dataUser.login && element.pass === dataUser.pass);
+    if (dataUsers.users.find((element: any) => element.login === dataUser.login && element.pass === dataUser.pass)) return {"ok": true}
+    else return {"ok": false}
 }
 
 export function registerUser(dataUser: any) {
-    let result = false;
-    if(!dataUsers.users.find((element: any) => element.login === dataUser.login)) {
+    let result = {
+        "ok": false
+    }
+    if(!dataUsers.users.find((element: any) => element.login.toString() === dataUser.login.toString())) {
         dataUsers.users.push(dataUser);
         fs.writeFileSync('users.json', JSON.stringify(dataUsers));
-        result = true;
+        result.ok = true;
+        return result;
     }
     return result;
 }
