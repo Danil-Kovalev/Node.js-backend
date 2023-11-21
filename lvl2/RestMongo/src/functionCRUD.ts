4f8d4bb452ce6e7967d8c4d9c9a5be18e6353f1c
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-11-21 22:40:43 +0200

    fixed getData

--- lvl2/RestMongo/src/functionCRUD.ts
+++ lvl2/RestMongo/src/functionCRUD.ts
@@ -1,32 +1,33 @@
 import { Request, Response } from 'express';
 import data from '../data.json';
 import * as fs from 'fs';
-import { collectionDb } from './mongoDatabase';
+// import { collectionDb } from './mongoDatabase';
 import { getUser } from './checkUsers';
 
 export function getData(req: Request, res: Response) {
     let userItemId = getUser(req.session.login);
     if (userItemId !== -1) {
-        collectionDb.countDocuments().then(result => {
-            if (result == 0) collectionDb.insertMany(data.users);
-        })
+        // collectionDb.countDocuments().then(result => {
+        //     if (result == 0) collectionDb.insertMany(data.users);
+        // })
         res.send(JSON.stringify(data.users[userItemId]));
     }
     else {
-        res.sendStatus(500).send({"error": "Unknown data"})
+        res.send({"error": "forbidden"})
     }
 }
 
 export function addData(req: Request, res: Response) {
     let userItemId = getUser(req.session.login);
-    let newData = {
+    let oldData = data.users[userItemId];
+    let newItem = {
         id: data.users[userItemId].items.length + 1,
         text: req.body.text.toString(),
         checked: true
     }
-    data.users[userItemId].items.push(newData);
-    collectionDb.insertOne(newData); // why writing __id
-    fs.writeFileSync('data.json', JSON.stringify(data));
+    data.users[userItemId].items.push(newItem);
+    // collectionDb.updateOne(oldData, {$set: {oldData: data.users[userItemId]}})
+    fs.writeFileSync('data.json', JSON.stringify(data))
     res.send(JSON.stringify({"id": data.users[userItemId].items.length}));
 }
 
@@ -37,7 +38,7 @@ export function updateData(req: Request, res: Response) {
     let result = {
         "ok": false
     }
-    collectionDb.updateOne({id: dataUser.id}, {$set: {text: dataUser.text.toString()}});
+    // collectionDb.updateOne({id: dataUser.id}, {$set: {text: dataUser.text.toString()}});
     data.users[userItemId].items.forEach((element: any) => {
         if (element.id.toString() === idUser) {
             data.users[userItemId].items[element.id - 1] = dataUser;
@@ -55,7 +56,7 @@ export function deleteData(req: Request, res: Response) {
     let result = {
         "ok": false
     }
-    collectionDb.deleteOne({id: dataUser.id});
+    // collectionDb.deleteOne({id: dataUser.id});
     data.users[userItemId].items.forEach((element: any) => {
         if (element.id === dataUser.id) {
             data.users[userItemId].items.splice(element.id - 1, 1);
