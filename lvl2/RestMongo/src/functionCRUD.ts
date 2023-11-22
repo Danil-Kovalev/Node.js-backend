1dcae1853cc3872f0341ba302646f9008dbf4926
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-11-22 21:02:28 +0200

    added mongo database and added add, update and delete functionality

--- lvl2/RestMongo/src/functionCRUD.ts
+++ lvl2/RestMongo/src/functionCRUD.ts
@@ -1,15 +1,15 @@
 import { Request, Response } from 'express';
 import data from '../data.json';
 import * as fs from 'fs';
-// import { collectionDb } from './mongoDatabase';
+import { collectionDb } from './mongoDatabase';
 import { getUser } from './checkUsers';
 
 export function getData(req: Request, res: Response) {
     let userItemId = getUser(req.session.login);
     if (userItemId !== -1) {
-        // collectionDb.countDocuments().then(result => {
-        //     if (result == 0) collectionDb.insertMany(data.users);
-        // })
+        collectionDb.countDocuments().then(result => {
+            if (result == 0) collectionDb.insertMany(data.users);
+        })
         res.send(JSON.stringify(data.users[userItemId]));
     }
     else {
@@ -19,14 +19,13 @@ export function getData(req: Request, res: Response) {
 
 export function addData(req: Request, res: Response) {
     let userItemId = getUser(req.session.login);
-    let oldData = data.users[userItemId];
     let newItem = {
         id: data.users[userItemId].items.length + 1,
         text: req.body.text.toString(),
         checked: true
     }
     data.users[userItemId].items.push(newItem);
-    // collectionDb.updateOne(oldData, {$set: {oldData: data.users[userItemId]}})
+    collectionDb.updateOne({login: req.session.login}, {$push: {items: newItem}});
     fs.writeFileSync('data.json', JSON.stringify(data))
     res.send(JSON.stringify({"id": data.users[userItemId].items.length}));
 }
@@ -38,13 +37,12 @@ export function updateData(req: Request, res: Response) {
     let result = {
         "ok": false
     }
-    // collectionDb.updateOne({id: dataUser.id}, {$set: {text: dataUser.text.toString()}});
+    collectionDb.updateOne({login: req.session.login, "items.id": dataUser.id}, {$set: {"items.$.text": dataUser.text}});
     data.users[userItemId].items.forEach((element: any) => {
         if (element.id.toString() === idUser) {
             data.users[userItemId].items[element.id - 1] = dataUser;
             fs.writeFileSync('data.json', JSON.stringify(data));
             result.ok = true;
-            return JSON.stringify(result);
         }
     });
     res.send(JSON.stringify(result));
@@ -56,13 +54,12 @@ export function deleteData(req: Request, res: Response) {
     let result = {
         "ok": false
     }
-    // collectionDb.deleteOne({id: dataUser.id});
+    collectionDb.updateOne({login: req.session.login, "items.id": dataUser.id}, {$pull: {items: {id: dataUser.id}}});
     data.users[userItemId].items.forEach((element: any) => {
         if (element.id === dataUser.id) {
             data.users[userItemId].items.splice(element.id - 1, 1);
             fs.writeFileSync('data.json', JSON.stringify(data));
             result.ok = true;
-            return JSON.stringify(result);
         }
     })
     res.send(JSON.stringify(result));
