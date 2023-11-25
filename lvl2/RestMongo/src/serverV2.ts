cd62b41dd1548d03a4769602313dda384031ff2c
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-11-25 21:10:12 +0200

    added realization authentication and functions CRUD

--- lvl2/RestMongo/src/serverV2.ts
+++ lvl2/RestMongo/src/serverV2.ts
@@ -1,8 +1,11 @@
-import express, { Express, Request, Response, query } from 'express';
+import express, { Request, Response } from 'express';
 import * as path from 'path';
 import bodyParser from "body-parser";
+
 import session from 'express-session';
 import FileStore from 'session-file-store';
+import cors from 'cors';
+
 import { getAction } from './functionActions';
 
 const app = express();
@@ -11,6 +14,11 @@ const port: number = 3005;
 const FileStoreSession = FileStore(session);
 const jsonParser = bodyParser.json();
 
+app.use(cors({
+  origin: 'http://localhost:8080',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}))
 app.use(express.static(path.resolve(dirname, 'front')));
 app.use(
     session({
@@ -18,7 +26,7 @@ app.use(
         path: "./sessions"
       }),
       secret: 'keyboard cat',
-      resave: true,
+      resave: false,
       saveUninitialized: true,
       cookie: {
         path: "/",
@@ -38,31 +46,12 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.post('/api/v2/router', jsonParser, (req: Request, res: Response) => {
-  let resultAction: any;
-  if (Object.keys(req.body).length !== 0) {
-    resultAction = getAction(req.query.action, req.body);
-    if (req.query.action === "register") {
-      let loginUser = req.body;
-      req.session.login = loginUser.login;
-      if (resultAction.ok) res.send(resultAction)
-      else res.send({"error": "User not exist"})
-    }
-    else if (req.query.action === "login") {
-      if (resultAction.ok) res.send(resultAction)
-      else res.send({"error": "User not exist"})
-    }
+  let resultQs = getAction(req.query.action);
+  if (typeof resultQs === "function") {
+    resultQs(req, res);
   }
   else {
-    if (req.query.action === "logout") {
-      req.session.destroy((err) => {
-        if (err) throw Error;
-        res.clearCookie('connect.sid');
-        res.send({"ok": true});
-      })
-    }
-    else {
-      res.send(getAction(req.query.action));
-    }
+    res.send(resultQs);
   }
 });
 
