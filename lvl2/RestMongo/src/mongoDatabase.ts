f8636ab46a438add641717b28119888ebbd21070
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-12-07 19:51:54 +0200

    delete key to db

--- lvl2/RestMongo/src/mongoDatabase.ts
+++ lvl2/RestMongo/src/mongoDatabase.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const dbUrl = "mongodb+srv://Danylo:passwordcourses12345@datauser.tqkhlcr.mongodb.net/";
+const dbUrl = "";
 const client = new MongoClient(dbUrl);
 client.connect().then(res => console.log("Connected to database!"));
 const db = client.db("myDatabase");
