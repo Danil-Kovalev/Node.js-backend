6fcc44dbfedfc52802d035a70a071096d658df0c
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-12-16 19:17:47 +0200

    added book opening request handler

--- lvl3/MVC/back/src/serverV1.ts
+++ lvl3/MVC/back/src/serverV1.ts
@@ -9,6 +9,20 @@ const dirname: string = path.resolve();
 const PORT: number = 3000;
 const jsonParser = bodyParser.json();
 
+let dataBooks = {
+    data: {
+        books:
+        [
+            { id: 1, title: 'СИ++ И КОМПЬЮТЕРНАЯ ГРАФИКА', author: 'Андрей Богуславский',  description: 'Лекции и практикум по программированию на Си++', year: 2003, pages: 200, event: false},
+            { id: 2, title: 'Программирование на языке Go!', author: 'Марк Саммерфильд', description: 'Лекції и практимум мовою Go', year: 2001, pages: 300, event: false },
+            { id: 3, title: 'Толковый словарь сетевых терминов и аббревиатур', author: 'М. Вильямс', description: 'Словар', year: 2005, pages: 100, event: false}
+        ],
+        total: {
+            amount: 3
+        }
+    }
+}
+
 app.use(express.static(path.join(dirname, '/front')));
 app.use(jsonParser)
 
@@ -20,28 +34,15 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.get('/book/:bookID', (req: Request, res: Response) => {
-    // console.log("Params1, /book/:bookID --- " + Object.entries(req.params));
     res.render(path.join(dirname, '/views/book-page.ejs'))
 })
 
 app.get('/api/v1/books', (req: Request, res: Response) => {
-    let dataBooks = {
-        data: {
-            books:
-            [
-                { id: 23, title: 'Программирование на языке Go!', author: 'Марк Саммерфильд' },
-                { id: 25, title: 'Толковый словарь сетевых терминов и аббревиатур', author: 'М. Вильямс'}
-            ],
-            total: {
-                amount: 2
-            }
-        }
-    }
     res.send(dataBooks)
 })
 
 app.get('/api/v1/book/:bookID', (req: Request, res: Response) => {
-//    console.log("Params3, /api/v1/book/:bookID --- " + Object.entries(req.params));
+    res.send(dataBooks.data.books[Number(Object.values(req.params)) - 1]);
 })
 
 app.listen(PORT, () => {
