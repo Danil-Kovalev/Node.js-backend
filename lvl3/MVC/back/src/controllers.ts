218cf710e8ac964b6897b0ff3126d8f152a9d80d
Danylo Kovalov <danilkovalev1306@gmail.com>
2023-12-26 19:38:58 +0200

    added data from SQL database

--- lvl3/MVC/back/src/controllers.ts
+++ lvl3/MVC/back/src/controllers.ts
@@ -1,22 +1,26 @@
 import dataBooks from '../books.json';
 import { Request, Response } from 'express';
+import { clientDB } from '../../MySQL_DB/clientDB';
 
 const DEFAULT_FILTER = 'new';
 const DEFAULT_OFFSET = 0;
 
 export function getBooks(req: Request, res: Response) {
-    let dataReady = {
-        data: {
-            books: dataBooks.data.books.slice(convertOffset(Number(req.query.offset)), Number(req.query.limit) + convertOffset(Number(req.query.offset))),
-            total: {
-                amount: dataBooks.data.books.length
+    let dataReady;
+    clientDB.query("SELECT * FROM books", (err: Error, result: Array<object>) => {
+        dataReady = {
+            data: {
+                books: result.slice(convertOffset(Number(req.query.offset)), Number(req.query.limit) + convertOffset(Number(req.query.offset))),
+                total: {
+                    amount: result.length
+                },
+                filter: convertFilter(String(req.query.filter)),
+                offset: convertOffset(Number(req.query.offset))
             },
-            filter: convertFilter(String(req.query.filter)),
-            offset: convertOffset(Number(req.query.offset))
-        },
-        success: true
-    }    
-    res.send(dataReady)
+            success: true
+        }
+        res.send(dataReady)
+    })
 }
 
 function convertFilter(valueReqData: string | undefined): string {
@@ -28,6 +32,15 @@ function convertOffset(valueReqData: number | undefined): number {
 }
 
 export function getBook(req: Request, res: Response) {
-    let indexBook = Number(Object.values(req.params)) - 1;
-    res.send(dataBooks.data.books[indexBook]);
+    let indexBook = Number(Object.values(req.params));
+    let dataReady;
+    clientDB.query(`SELECT * FROM books WHERE id=${indexBook}`, (err: Error, result: Array<object>) => {
+        dataReady = {
+            data: {
+                book: result[0],
+            },
+            success: true
+        }
+        res.send(dataReady)
+    })
 }
