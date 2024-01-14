a2f011efc1a8687996d91505665ce3cdcf75e9ad
Danylo Kovalov <danilkovalev1306@gmail.com>
2024-01-14 22:02:44 +0200

    add constant for config connect to database

--- lvl3/MVC/back/src/constants.ts
+++ lvl3/MVC/back/src/constants.ts
@@ -2,6 +2,13 @@ import { authorizer } from "./controllers";
 
 export const DEFAULT_FILTER = 'new';
 export const DEFAULT_OFFSET = 0;
+export const CONFIG = {
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    database: "dataBooks",
+    password: ""
+}
 
 export const optionsAuth = {
     users: { 'admin': '123' },
