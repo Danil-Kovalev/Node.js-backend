482ccce45c2a90cebe41ece72ce02921903f5b7a
Danylo Kovalov <danilkovalev1306@gmail.com>
2024-01-18 20:23:51 +0200

    change routers for user page with mysql promise

--- lvl3/MVC/back/src/controllers.ts
+++ lvl3/MVC/back/src/controllers.ts
@@ -1,28 +1,30 @@
 import { Request, Response } from 'express';
-import { clientDB } from '../../MySQL_DB/clientDB';
-import basicAuth, { IBasicAuthedRequest } from 'express-basic-auth';
-import { DEFAULT_OFFSET } from './constants';
-import { DEFAULT_FILTER } from './constants';
+import { db } from '../../MySQL_DB/db.js';
+import basicAuth from 'express-basic-auth';
+import { DEFAULT_OFFSET } from './constants.js';
+import { DEFAULT_FILTER } from './constants.js';
+import { getAllBooks, getBookById } from './scripts.js';
 
-export function getBooks(req: Request, res: Response) {
-    let convertedOffset = convertOffset(Number(req.query.offset));
-    let convertedFilter = convertFilter(String(req.query.filter));
+export async function getBooks(req: Request, res: Response) {
+    let convertedOffset: number = convertOffset(Number(req.query.offset));
+    let convertedFilter: string = convertFilter(String(req.query.filter));
     let dataReady;
-    
-    clientDB.query("SELECT * FROM books", (err: Error, result: Array<object>) => {
-        dataReady = {
-            data: {
-                books: result.slice(convertedOffset, Number(req.query.limit) + convertedOffset),
-                total: {
-                    amount: result.length
-                },
-                filter: convertedFilter,
-                offset: convertedOffset
+
+    let result = await getAllBooks();
+
+    dataReady = {
+        data: {
+            books: result.slice(convertedOffset, Number(req.query.limit) + convertedOffset),
+            total: {
+                amount: result.length
             },
-            success: true
-        }
-        res.send(dataReady);
-    })
+            filter: convertedFilter,
+            offset: convertedOffset
+        },
+        success: true
+    }
+
+    res.send(dataReady);
 }
 
 function convertOffset(valueReqData: number): number {
@@ -33,24 +35,25 @@ function convertFilter(valueReqData: string): string {
     return valueReqData === undefined ? DEFAULT_FILTER : String(valueReqData);
 }
 
-export function getBook(req: Request, res: Response) {
+export async function getBook(req: Request, res: Response) {
     let indexBook = Number(Object.values(req.params));
     let dataReady;
-    clientDB.query(`SELECT * FROM books WHERE id=${indexBook}`, (err: Error, result: Array<object>) => {
-        dataReady = {
-            data: {
-                book: result[0],
-            },
-            success: true
-        }
-        res.send(dataReady)
-    })
+
+    let result = await getBookById(indexBook);
+
+    dataReady = {
+        data: {
+            book: result[0],
+        },
+        success: true
+    }
+    res.send(dataReady)
 }
 
 export function authorizer(username: string, password: string) {
-    const userMatches = basicAuth.safeCompare(username, '')
-    const passwordMatches = basicAuth.safeCompare(password, '')
-    
+    const userMatches = basicAuth.safeCompare(username, 'admin')
+    const passwordMatches = basicAuth.safeCompare(password, '1234')
+
     return userMatches && passwordMatches
 }
 
