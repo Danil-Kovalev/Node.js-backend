c1832e67992d20358b6aeb72dcbbb6af8b5cc3e7
Danylo Kovalov <danilkovalev1306@gmail.com>
2024-01-27 18:33:34 +0200

    added sort by filter new, popular and all books

--- lvl3/MVC/back/src/controllers.ts
+++ lvl3/MVC/back/src/controllers.ts
@@ -1,16 +1,24 @@
 import { Request, Response } from 'express';
-import { db } from '../../MySQL_DB/db.js';
 import basicAuth from 'express-basic-auth';
 import { DEFAULT_OFFSET } from './constants.js';
 import { DEFAULT_FILTER } from './constants.js';
-import { getAllBooks, getBookById } from './scripts.js';
+import { getAllBooks, getBookById, getNewBooks, getPopularBooks, increaseClick, increaseViews } from './scripts.js';
 
-export async function getBooks(req: Request, res: Response) {
+export async function getBooks(req: Request, res: Response) {    
     let convertedOffset: number = convertOffset(Number(req.query.offset));
     let convertedFilter: string = convertFilter(String(req.query.filter));
     let dataReady;
-
-    let result = await getAllBooks();
+    let result;
+    if (convertedFilter === 'new') {
+        result = await getNewBooks();
+    }
+    else 
+    if (convertedFilter === 'popular') {
+        result = await getPopularBooks();
+    }
+    else {
+        result = await getAllBooks();
+    }
 
     dataReady = {
         data: {
@@ -23,7 +31,7 @@ export async function getBooks(req: Request, res: Response) {
         },
         success: true
     }
-
+    
     res.send(dataReady);
 }
 
@@ -41,6 +49,8 @@ export async function getBook(req: Request, res: Response) {
 
     let result = await getBookById(indexBook);
 
+    await increaseViews(indexBook);
+ 
     dataReady = {
         data: {
             book: result[0],
@@ -48,6 +58,13 @@ export async function getBook(req: Request, res: Response) {
         success: true
     }
     res.send(dataReady)
+}
+
+export async function addClick(req: Request, res: Response) {
+    let idBook = Number(Object.values(req.params));
+    let result = await increaseClick(idBook);
+
+    res.send({"success": result});
 }
 
 export function authorizer(username: string, password: string) {
