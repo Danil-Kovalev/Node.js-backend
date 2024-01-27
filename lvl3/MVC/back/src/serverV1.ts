5702178786b56dcd256c462155cb3a1c445919f5
Danylo Kovalov <danilkovalev1306@gmail.com>
2024-01-27 18:31:15 +0200

    added multer for loading img to folder images from front folder

--- lvl3/MVC/back/src/serverV1.ts
+++ lvl3/MVC/back/src/serverV1.ts
@@ -2,20 +2,33 @@ import express, { Express, Request, Response } from 'express';
 import * as path from 'path';
 import bodyParser from "body-parser";
 import basicAuth from 'express-basic-auth';
+import multer from 'multer';
 
 import * as router from './controllers.js';
 import * as adminRouter from './adminController.js';
 import { optionsAuth } from './constants.js';
 import { backupData } from '../cron/cron.js';
 
-await backupData();
-
 const app: Express = express();
 
 const dirname: string = path.resolve();
 
+
 const PORT: number = 3000;
 const jsonParser = bodyParser.json();
+
+await backupData();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve('./front/images'))
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  })
+  
+const upload = multer({ storage: storage })
 
 app.use(express.static(path.join(dirname, '/front')));
 app.use(jsonParser)
@@ -36,6 +49,7 @@ app.get('/book/:bookID', (req: Request, res: Response) => {
 })
 
 app.route('/api/v1/book/:bookID').get(router.getBook);
+app.route('/api/v1/book/click/:bookID').get(router.addClick);
 app.route('/api/v1/books').get(router.getBooks);
 
 app.route('/admin/api/v1/books').get(adminRouter.getBooks);
@@ -43,6 +57,10 @@ app.route('/admin/api/v1/books').get(adminRouter.getBooks);
 app.route('/admin/api/v1/book')
 .delete(adminRouter.deleteBookAdmin)
 .post(adminRouter.addBookAdmin)
+
+app.post('/admin/api/v1/image', upload.single('new-img'), (req: Request, res: Response) => {
+    res.send({"success": true})
+})
 
 app.listen(PORT, () => {
     console.log(`Server V1 starts on port ${PORT}`);
