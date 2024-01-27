bacb911a909f523d7356a384dd44be6b626fed80
Danylo Kovalov <danilkovalev1306@gmail.com>
2024-01-27 18:34:13 +0200

    added possibility add few authors in one book

--- lvl3/MVC/back/src/scripts.ts
+++ lvl3/MVC/back/src/scripts.ts
@@ -1,6 +1,6 @@
-import { FieldPacket, RowDataPacket } from 'mysql2';
+import { RowDataPacket } from 'mysql2';
 import { db } from '../../MySQL_DB/db.js';
-import { readFile, writeFile } from 'fs/promises';
+import { readFile } from 'fs/promises';
 import { PATH_SQL } from './constants.js';
 
 async function getSqlRequest(fileName: string, type: string): Promise<string> {
@@ -9,7 +9,21 @@ async function getSqlRequest(fileName: string, type: string): Promise<string> {
 
 export async function getAllBooks(): Promise<RowDataPacket[]> {
     let query = await getSqlRequest('get-all-books', 'upData');
-    
+
+    let result = await db.execute<RowDataPacket[]>(query);
+    return result[0];
+}
+
+export async function getPopularBooks(): Promise<RowDataPacket[]> {
+    let query = await getSqlRequest('get-popular-books', 'upData');
+
+    let result = await db.execute<RowDataPacket[]>(query);
+    return result[0];
+}
+
+export async function getNewBooks(): Promise<RowDataPacket[]> {
+    let query = await getSqlRequest('get-new-books', 'upData');
+
     let result = await db.execute<RowDataPacket[]>(query);
     return result[0];
 }
@@ -21,22 +35,24 @@ export async function getBookById(index: number): Promise<RowDataPacket[]> {
     return result[0];
 }
 
-export async function addBook(name:string, author: string, description: string, year: string, pages: string) {
+export async function addBook(name: string, author: Array<string>, description: string, year: string, pages: string, date: string) {
     let queryAddBook = await getSqlRequest('add-book', 'upData')
 
     let countBooks = await getCountBooks();
-    let countAuthors = await getCountAuthors();
 
     let idBook = countBooks[0].count;
     idBook++;
 
-    let idAuthor = countAuthors[0].count;
-    idAuthor++;
+    await db.execute<RowDataPacket[]>(queryAddBook, [idBook, name, description, year, pages, date]);
 
-    await addAuthors(idAuthor, author);
+    let countAuthors = await getCountAuthors();
+    let idAuthor: number = countAuthors[0].count;
 
-    await db.execute<RowDataPacket[]>(queryAddBook, [idBook, name, description, year, pages]);
-    await addBooksAuthors(idBook, idAuthor);
+    author.map((el: string) => {
+        idAuthor++;
+        addAuthors(idAuthor, el);
+        addBooksAuthors(idBook, idAuthor);
+    })
 }
 
 async function getCountBooks(): Promise<RowDataPacket[]> {
@@ -65,42 +81,6 @@ async function addBooksAuthors(idBook: number, idAuthor: number) {
     await db.execute<RowDataPacket[]>(query, [idBook, idAuthor]);
 }
 
-export async function createBooksTable() {
-    let query = await getSqlRequest('create-books-table', 'upData')
-
-    await db.execute<RowDataPacket[]>(query);
-}
-
-export async function createAuthorsTable() {
-    let query = await getSqlRequest('create-authors-table', 'upData')
-
-    await db.execute<RowDataPacket[]>(query);
-}
-
-export async function createBooksAuthorsTable() {
-    let query = await getSqlRequest('create-books-authors-table', 'upData')
-
-    await db.execute(query);
-}
-
-export async function deleteBooksTable() {
-    let query = await getSqlRequest('delete-books-table', 'downData')
-
-    await db.execute(query);
-}
-
-export async function deleteAuthorsTable() {
-    let query = await getSqlRequest('delete-authors-table', 'downData')
-
-    await db.execute(query);
-}
-
-export async function deleteBooksAuthorsTable() {
-    let query = await getSqlRequest('delete-books-authors-table', 'downData')
-
-    await db.execute(query);
-}
-
 export async function deleteBook(id: number) {
     let query = await getSqlRequest('delete-book', 'upData')
 
@@ -113,3 +93,26 @@ async function deleteBooksAuthors(idBook: number) {
 
     await db.execute<RowDataPacket[]>(query, [idBook]);
 }
+
+export async function increaseClick(idBook: number): Promise<boolean> {
+    let result = false;
+    let query = await getSqlRequest('increase-clicks-book', 'upData')
+
+    if (await db.execute(query, [idBook])) {
+        result = true;
+    };
+
+    return result;
+}
+
+export async function increaseViews(idBook: number): Promise<boolean> {
+    let result = false;
+    let query = await getSqlRequest('increase-views-book', 'upData')
+
+    if (await db.execute(query, [idBook])) {
+        result = true;
+    };
+
+    return result;
+}
+
